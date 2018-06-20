7b4942a65a6147cf3fa939078f61493c61b282d5
Jason Foster <jason@designspin.co.uk>
2018-06-20 01:36:32 +0100

    Added material components to account and home;

--- src/pages/Account.js
+++ src/pages/Account.js
@@ -1,21 +1,29 @@
 import React from 'react';
-
-import AuthUserContext from '../components/AuthUserContext';
+import { connect } from 'react-redux';
+import { compose } from 'recompose';
+import Typography from '@material-ui/core/Typography';
 import { PasswordForgetForm } from './PasswordForget';
 import PasswordChangeForm from '../components/PasswordChange';
 import withAuthorisation from '../components/withAuthorisation';
 
-const AccountPage = () =>
-  <AuthUserContext.Consumer>
-    {authUser =>
-      <div>
-        <h1>Account: {authUser.email}</h1>
-        <PasswordForgetForm />
-        <PasswordChangeForm />
-      </div>
-    }
-  </AuthUserContext.Consumer>
+const AccountPage = ({ authUser }) =>
+  <div>
+    <Typography
+      gutterBottom
+      variant="headline" 
+      component="h1"
+      >Account: {authUser.email}</Typography>
+    <PasswordForgetForm />
+    <PasswordChangeForm />
+  </div>
+
+const mapStateToProps = (state) => ({
+  authUser: state.sessionState.authUser,
+})
 
 const authCondition = (authUser) => !!authUser;
 
-export default withAuthorisation(authCondition)(AccountPage);
+export default compose(
+  withAuthorisation(authCondition),
+  connect(mapStateToProps)
+)(AccountPage);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,15 @@
 import React from 'react';
-
+import Typography from '@material-ui/core/Typography';
 import withAuthorisation from '../components/withAuthorisation';
 
 const HomePage = () =>
   <div>
-    <h1>Home Page</h1>
-    <p>This page is accesible to any signed in user.</p>
+    <Typography
+      gutterBottom
+      variant="headline" 
+      component="h1"
+      >Home Page</Typography>
+    <Typography>This page is accesible to any signed in user.</Typography>
   </div>
 
 const authCondition = (authUser) => !!authUser;
